e3248781b0f7077c141afd535130fe48a2c6bb93
Kyle Hoc <kjhoch2@gmail.com>
2024-07-17 22:02:42 -0400

    made a mistake, dealing with local changes

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("dishes/", views.DishListCreate.as_view(), name="dish-list"),
-    path("dishes/delete/<int:pk>/", views.DishDelete.as_view(), name="delete-dish")
-]
- 
--- frontend/src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-function NotFound() {
-    return <div>
-        <h1>The page you were looking for does not exist</h1>
-    </div>
-}
-
-export default NotFound
